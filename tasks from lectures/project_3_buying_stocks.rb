a06e7ca2064e00cbe67236a6ca42d8d9826f0d4e
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-03-18 13:27:02 +0200

    implemented method for finding best buy, sell stocks day

---
+++ tasks from lectures/project_3_buying_stocks.rb
@@ -0,0 +1,17 @@
+def stock_picker(input_array)
+  information = {}
+  for i in 0..input_array.length - 1
+    for ii in (i + 1)..(input_array.length - 1)
+      information[[i, ii]] = [input_array[i], input_array[ii]]
+    end
+  end
+  best_pair = { [0, 0] => 0 }
+  information.each do |key, value|
+    # print "my pair =#{value[1] - value[0]}, and standard result = #{best_pair.values[0][1] - best_pair.values[0][0]}"
+    best_pair = { key => value } if value[1] - value[0] > best_pair.values[0][1] - best_pair.values[0][0]
+  end
+  best_pair.keys.first
+end
+
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# => [1, 4]   for a profit of $15 - $3 == $12
